b21d185c2e44faa50203218c985d146f577c6822
Jaeho Yoo <y73447jh@gmail.com>
2024-08-31 17:10:23 +0900

    AppStorage 는 애플이 만들어준 편리한 프로퍼티 래퍼

--- UserDefaultsPro/ContentView.swift
+++ UserDefaultsPro/ContentView.swift
@@ -7,27 +7,18 @@
 
 import SwiftUI
 
-// 어떻게 하면 앱을 껐다 켜도 남아있는 데이터를 만들 수 있을까?
-// -> "Persistency"
+// UserDefaults 와 SwiftUI 는 같이 쓰기 적합하지 않다.
+// 이를 해결하기 위해 애플이 iOS 14 에서 @AppStorage 를 만들어줬다.
 
 struct ContentView: View {
 
-  @State private var isToggleOn: Bool = UserDefaults.standard.bool(forKey: "isToggleOn")
+//  @State private var isToggleOn: Bool = UserDefaults.standard.bool(forKey: "isToggleOn")
+  @AppStorage("isToggleOn") private var isToggleOn: Bool = false
+  // 이렇게 생긴 이니셜라이저도 있음ㅋ 디폴트 값
+  @AppStorage(wrappedValue: false, "isToggleOn2") private var isToggleOn2: Bool
 
   var body: some View {
     VStack(spacing: 60) {
-      GroupBox("유저디폴트") {
-        // 존재하지 않는 키값 조회 -> String 타입
-        Text("\(UserDefaults.standard.string(forKey: "notExists"))")
-
-        // 존재하지 않는 키값 조회 -> Bool 타입
-        Text("\(UserDefaults.standard.bool(forKey: "notExists"))")
-
-        // 존재하지 않는 키값 조회 -> Int 타입
-        Text("\(UserDefaults.standard.integer(forKey: "notExists"))")
-      }
-      .font(.largeTitle)
-
       Toggle(
         "\(isToggleOn ? "토글 ON" : "토글 OFF")",
         systemImage: isToggleOn ? "lightbulb.fill" : "lightbulb",
@@ -38,8 +29,8 @@ struct ContentView: View {
       .frame(width: 200)
 
       Button("토글 뒤집기") {
-        // !!!: 분명 값을 바꿨지만 View 가 다시 그려지지 않는다. 재실행하면 반영됨!
-        UserDefaults.standard.set(!isToggleOn, forKey: "isToggleOn")
+        // @State 사용하듯이 유저디폴트를 쓸 수 있다. 값이 변경되면 View 도 자동으로 다시 그려짐!
+        self.isToggleOn.toggle()
       }
       .font(.title)
       .buttonStyle(.borderedProminent)

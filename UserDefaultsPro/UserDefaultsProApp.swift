942253f73196389039571a417302096ac92e5a44
Jaeho Yoo <y73447jh@gmail.com>
2024-08-31 16:25:57 +0900

    Initial Commit

---
+++ UserDefaultsPro/UserDefaultsProApp.swift
@@ -0,0 +1,17 @@
+//
+//  UserDefaultsProApp.swift
+//  UserDefaultsPro
+//
+//  Created by 유재호 on 8/31/24.
+//
+
+import SwiftUI
+
+@main
+struct UserDefaultsProApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

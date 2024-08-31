6684bfdb9c8891de0c291af278086322b2663455
Jaeho Yoo <y73447jh@gmail.com>
2024-08-31 16:58:04 +0900

    UserDefaults 를 쌩으로 쓰면 SwiftUI 랑 어울리지 않음

--- UserDefaultsPro/ContentView.swift
+++ UserDefaultsPro/ContentView.swift
@@ -7,14 +7,26 @@
 
 import SwiftUI
 
+// 어떻게 하면 앱을 껐다 켜도 남아있는 데이터를 만들 수 있을까?
+// -> "Persistency"
+
 struct ContentView: View {
 
-  @State private var isToggleOn: Bool = false
+  @State private var isToggleOn: Bool = UserDefaults.standard.bool(forKey: "isToggleOn")
 
   var body: some View {
-    VStack {
-      Text("앱을 껐다 켜도 Toggle 상태가 유지될까?")
-        .font(.largeTitle)
+    VStack(spacing: 60) {
+      GroupBox("유저디폴트") {
+        // 존재하지 않는 키값 조회 -> String 타입
+        Text("\(UserDefaults.standard.string(forKey: "notExists"))")
+
+        // 존재하지 않는 키값 조회 -> Bool 타입
+        Text("\(UserDefaults.standard.bool(forKey: "notExists"))")
+
+        // 존재하지 않는 키값 조회 -> Int 타입
+        Text("\(UserDefaults.standard.integer(forKey: "notExists"))")
+      }
+      .font(.largeTitle)
 
       Toggle(
         "\(isToggleOn ? "토글 ON" : "토글 OFF")",
@@ -24,6 +36,13 @@ struct ContentView: View {
       .symbolEffect(.bounce, value: isToggleOn) // iOS 17+
       .font(.title)
       .frame(width: 200)
+
+      Button("토글 뒤집기") {
+        // !!!: 분명 값을 바꿨지만 View 가 다시 그려지지 않는다. 재실행하면 반영됨!
+        UserDefaults.standard.set(!isToggleOn, forKey: "isToggleOn")
+      }
+      .font(.title)
+      .buttonStyle(.borderedProminent)
     }
   }
 }
